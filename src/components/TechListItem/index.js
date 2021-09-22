ab9a77c52b07b15ee37424ca54312ff997087f91
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-21 19:35:10 -0500

    created techlistitems index.js

---
+++ src/components/TechListItem/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TechListItem(props) {
+    return props.techItems.map(technology => {
+        return <li className="techUsed">{technology}</li>
+    })
+}
+
+export default TechListItem;
