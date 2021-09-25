8e56fba8af201cdb5962562682fa63df7d99b19b
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-25 10:54:10 -0500

    checking if last commit pushed

--- src/components/TechListItem/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function TechListItem(props) {
-    return props.techItems.map(technology => {
-        return <li className="techUsed">{technology}</li>
-    })
-}
-
-export default TechListItem;
